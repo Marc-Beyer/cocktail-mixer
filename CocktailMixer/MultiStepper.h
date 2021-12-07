7adee9c110f65b7f0a216ee37c302a298f4f0b67
Marc Beyer <marc.beyer@edu.bib.de>
2021-12-07 07:58:24 +0100

    Added position

--- CocktailMixer/MultiStepper.h
+++ CocktailMixer/MultiStepper.h
@@ -9,15 +9,26 @@ bool initStepper(int count);
 // for id, id >= 0 && id < motor-count 
 bool initMotor(int id, int *ports);
 
-// Call this in the loop-function to move the moter with the id at a speed
-// Returns true if motor is ready and false if the motor id is wrong or the motor is still moving.
-bool moveMotor(int id, long stepSpeed);
+// Call this in the loop-function to move the motor with the id at a speed
+// Returns 1 if motor is ready, -1 if the motor id is wrong and 0 the motor is still moving.
+int moveMotor(int id, long stepSpeed);
+
+/*
+ *  GETTER AND SETTER
+ */
+
+// Get the current position of the motor
+long getPosition(int id);
+
+// Reset the position of a motor if it's calibrated
+void setPosition(int id, long pos);
+
+// Get how "far" the motor will move
+long getStepsToGo(int id);
 
 // Define how far the motor with the id should move
 void setStepsToGo(int id, long steps);
 
-// Get e string with some stats about the motor with the id
-char* getStats(int id);
 #ifdef __cplusplus
 }
 #endif
