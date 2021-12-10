04911ba4a941b90d40c605ec0c0620606c659e45
Marco Kühn <marco.kuehn@edu.bib.de>
2021-12-10 14:51:37 +0100

    Added first draft get-request

---
+++ NodeServer/web/scripts/main.js
@@ -0,0 +1,26 @@
+const buttonList = document.querySelector(".buttons");
+const requestString = requestHTTP("localhost/4242/get-cocktails");
+
+console.log(buttonList);
+
+let drinkArray = JSON.parse(requestString);
+
+for (const drink of drinkArray) {
+   let li = document.createElement("li");
+   let button = document.createElement("button");
+   button.textContent = drink.name;
+
+   button.addEventListener("click", (e) => {
+      document.location = "./request-cocktail?name=" + drink.name;
+   });
+
+   li.appendChild(button);
+   buttonList.appendChild(li);
+}
+
+function requestHTTP(url) {
+   var xmlHttp = new XMLHttpRequest();
+   xmlHttp.open("GET", url, false);
+   xmlHttp.send(null);
+   return xmlHttp.responseText;
+}
