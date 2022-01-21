3ad5e94e661a0a4f0aa00ba9261ea042054bf4d4
Marc Beyer <marc.beyer.dev@gmail.com>
2022-01-21 20:19:17 +0100

    Critical button pin fix

--- NodeServer/modules/motorPath.js
+++ NodeServer/modules/motorPath.js
@@ -14,11 +14,10 @@ const LONG_DELAY = 4000;
 const MEDIUM_DELAY = 1000;
 const SHORT_DELAY = 1000;
 
-const RIGHT_BTN_ID = 11;
-const BOTTOM_BTN_ID = 12;
+const RIGHT_BTN_ID = 4;
+const BOTTOM_BTN_ID = 21;
 
 exports.getPathFromCocktail = (station, cocktail) => {
-
     let positions = [];
     for (const liquid of cocktail.liquids) {
         for (let index = 0; index < liquid.amount; index++) {
@@ -29,17 +28,25 @@ exports.getPathFromCocktail = (station, cocktail) => {
 
     let movingDistance = [(positions[0] - 1) * X_DIS_2 + X_DIS_2];
     for (let index = 1; index < positions.length; index++) {
-        movingDistance.push((positions[index] - positions[index - 1]) * X_DIS_2);
+        movingDistance.push(
+            (positions[index] - positions[index - 1]) * X_DIS_2
+        );
     }
     console.log("movingDistance", movingDistance);
 
     let path = [`${X_MOTOR_ID}:${X_DIS_1}:${X_MOTOR_SPEED}:${RIGHT_BTN_ID}:0`];
     for (let index = 0; index < movingDistance.length; index++) {
-        path.push(`${X_MOTOR_ID}:${movingDistance[index]}:${X_MOTOR_SPEED}:${RIGHT_BTN_ID}:${MEDIUM_DELAY}`);
-        path.push(`${Y_MOTOR_ID}:${-Y_DIS_1}:${X_MOTOR_SPEED}:${RIGHT_BTN_ID}:${LONG_DELAY}`);
-        path.push(`${Y_MOTOR_ID}:${Y_DIS_1}:${Y_MOTOR_SPEED}:${BOTTOM_BTN_ID}:${MEDIUM_DELAY}`);
+        path.push(
+            `${X_MOTOR_ID}:${movingDistance[index]}:${X_MOTOR_SPEED}:${RIGHT_BTN_ID}:${MEDIUM_DELAY}`
+        );
+        path.push(
+            `${Y_MOTOR_ID}:${-Y_DIS_1}:${X_MOTOR_SPEED}:${RIGHT_BTN_ID}:${LONG_DELAY}`
+        );
+        path.push(
+            `${Y_MOTOR_ID}:${Y_DIS_1}:${Y_MOTOR_SPEED}:${BOTTOM_BTN_ID}:${MEDIUM_DELAY}`
+        );
     }
     path.push(`-1:${MEDIUM_DELAY}`);
 
     return path;
-}
+};
